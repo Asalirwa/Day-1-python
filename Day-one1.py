128cf63c6ee867e2bd93bf94ddba8c8eb413f23f
Asalirwa <44318943+Asalirwa@users.noreply.github.com>
2018-11-14 17:25:49 +0300

    Add files via upload

---
+++ Day-one1.py
@@ -0,0 +1,8 @@
+year_of_birth = int(input("enter your year_of_birth"))
+if year_of_birth >= 2000 :
+  print ("minor")
+
+if year_of_birth >= 1982 :
+  print ("youth")
+
+else: print ("you are an adult")
